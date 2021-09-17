24f6d7a905c02b7115eb4f60631d37373e4b5078
Cecilia Baik <mb224dt@student.lnu.se>
2021-09-18 00:39:45 +0200

    Add Tokenizer.java class

---
+++ src/main/java/tokenizer/RegexTokenizer.java
@@ -0,0 +1,2 @@
+package tokenizer;public class RegexTokenizer {
+}
---
+++ src/main/java/tokenizer/Tokenizer.java
@@ -0,0 +1,43 @@
+package tokenizer;
+
+public class Tokenizer {
+
+    /**
+     * This constructor creates a new Tokenizer.
+     */
+    public Tokenizer() {}
+
+    /**
+     * This constructor accepts an argument of specified string.
+     */
+    public Tokenizer(String string) {}
+
+    /**
+     * This constructor accepts an argument of specified string and dot.
+     */
+    public Tokenizer(String string, String dot) {}
+
+    public void getActiveToken () {
+        // TODO: This method gets the active token which is the first token of a string.
+    }
+
+    public void getEndToken() {
+        // TODO: This method gets the end token of a string.
+    }
+
+    public void nextToken() {
+        // TODO: This method moves active token forward to next token.
+    }
+
+    public void previousToken() {
+        // TODO: This method moves active token backwards to previous token.
+    }
+
+    public void hasMoreTokens() {
+        // TODO: This method checks if there are more tokens available.
+    }
+
+    public void toString() {
+        // TODO: This method prints the result to the console.
+    }
+}
