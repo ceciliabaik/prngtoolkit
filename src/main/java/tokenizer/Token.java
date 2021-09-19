906ddfef9eda4e0b868572851ec0a1247d426c31
Cecilia Baik <mb224dt@student.lnu.se>
2021-09-19 21:23:42 +0200

    Add Token class

---
+++ src/main/java/tokenizer/Token.java
@@ -0,0 +1,30 @@
+package tokenizer;
+
+/**
+ * A string that holds a sequence of characters, like a word or a phrase.
+ */
+public class Token {
+    private String tokenName;
+    private Integer tokenValue;
+
+    public Token(String tokenName, Integer tokenValue) {
+        this.tokenName = tokenName;
+        this.tokenValue = tokenValue;
+    }
+
+    public String getTokenName() {
+        return tokenName;
+    }
+
+    public void setTokenName(String tokenName) {
+        this.tokenName = tokenName;
+    }
+
+    public Integer getTokenValue() {
+        return tokenValue;
+    }
+
+    public void setTokenValue(Integer tokenValue) {
+        this.tokenValue = tokenValue;
+    }
+}
