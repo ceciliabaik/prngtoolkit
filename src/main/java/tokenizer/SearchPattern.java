0553bc10396b438128bef88ccf5723add828558a
Cecilia Baik <mb224dt@student.lnu.se>
2021-09-24 00:07:49 +0200

    Add regex pattern for WordAndDotGrammar

--- src/main/java/tokenizer/SearchPattern.java
+++ src/main/java/tokenizer/SearchPattern.java
@@ -20,8 +20,8 @@ public class SearchPattern {
     public SearchPattern() {}
 
     public void defineSearchPattern() {
-      Pattern pattern = Pattern.compile("[a-zåäöA-ZÅÄÖ]+");
-      Matcher matcher = pattern.matcher("MeningenbeståravÖrd");
+      Pattern pattern = Pattern.compile("([a-zåäöA-ZÅÄÖ]+)(.+)(\\S+)");
+      Matcher matcher = pattern.matcher("Meningen består av ord.");
       boolean bool = matcher.matches();
       System.out.println(pattern);
       System.out.println(matcher);
