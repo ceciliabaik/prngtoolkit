c30d123740054ee2037eb56dab1d25606c824ece
Cecilia Baik <mb224dt@student.lnu.se>
2021-10-12 10:31:18 +0200

    Update token class

--- src/main/java/tokenizer/model/Token.java
+++ src/main/java/tokenizer/model/Token.java
@@ -1,21 +1,39 @@
 package tokenizer.model;
 
 public class Token {
-  private Integer tokenValue;
+  private TokenName name;
+  private int value;
 
   public enum TokenName {
-    IDENTIFIER, KEYWORD, SEPARATOR, OPERATOR, LITERAL, COMMENT, NUMBER, DIGIT
+    IDENTIFIER, KEYWORD, SEPARATOR, OPERATOR, LITERAL, COMMENT
   }
 
-  public Token(Integer tokenValue) {
-    this.tokenValue = tokenValue;
+  public Token(TokenName name, int value) {
+    this.name = name;
+    this.value = value;
   }
 
-  public Integer getTokenValue() {
-    return tokenValue;
+  public TokenName getName() {
+    return name;
   }
 
-  public void setTokenValue(Integer tokenValue) {
-    this.tokenValue = tokenValue;
+  public void setName(TokenName name) {
+    this.name = name;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+
+  @Override
+  public String toString() {
+    return "Token{" +
+            "name=" + name +
+            ", value=" + value +
+            '}';
   }
 }
