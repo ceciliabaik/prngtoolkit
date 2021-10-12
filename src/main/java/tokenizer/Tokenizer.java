738fa0d145668cf4656c8ca7e4440eefc163539a
Cecilia Baik <mb224dt@student.lnu.se>
2021-10-13 00:17:38 +0200

    Add Tokenizer methods

--- src/main/java/tokenizer/Tokenizer.java
+++ src/main/java/tokenizer/Tokenizer.java
@@ -1,8 +1,6 @@
 package tokenizer;
 
-import tokenizer.interpreter.RegularExpression;
 import tokenizer.interpreter.SourceCode;
-import tokenizer.model.Lexeme;
 import tokenizer.model.Token;
 
 import java.util.ArrayList;
@@ -12,21 +10,18 @@ import java.util.Arrays;
  * Tokenizer is a program that performs lexical analysis, which 
  * means a character-by-character evaluation of the input source 
  * code and identification of the tokens.
+ *  // private ArrayList<EnumMapRegex.TokenType> tokenTypeList;
  * 
  * @author Cecilia Baik
  */
 public class Tokenizer implements PublicInterface {
-  // private ArrayList<EnumMapRegex.TokenType> tokenTypeList;
-  private SourceCode sourceCode;
+  private final SourceCode sourceCode;
+  private final ArrayList<String> tokens;
   private boolean isActiveToken;
-  private Scanner scanner;
-  private Token token;
-  private Lexeme lexemeChar;
-  ArrayList<String> tokens = new ArrayList<>(Arrays.asList("Meningen", "består", "av", "ord", "."));
 
-  public Tokenizer(SourceCode sourceCode, Lexeme lexemeChar, Token token) {
+  public Tokenizer(SourceCode sourceCode, ArrayList<String> tokens) {
     this.sourceCode = sourceCode;
-    this.lexemeChar = lexemeChar;
+    this.tokens = new ArrayList<>(Arrays.asList("Meningen", "består", "av", "ord", "."));
   }
 
   public boolean isActiveToken() {
@@ -38,57 +33,58 @@ public class Tokenizer implements PublicInterface {
   }
 
   // Categorize into tokens.
-  public boolean performTokenization(LiteralExpression literalExpression) {
+  public void performTokenization() {
     getInitActiveToken();
     while (!sourceCode.getInputStream().isEmpty()) {
       if (lexemeChar.hasWhiteSpace()) {
         ignoreWhiteSpace();
         continue;
       } else if (literalExpression.interpret()) {
-        return token(Token.TokenType.LITERAL, token.getValue());
+        return getToken(Token.TokenType.LITERAL, token.getValue());
       } else if (digitExpression.interpret()) {
-        Token token = token(Token.TokenType.LITERAL, token.getValue());
-      }
+        return getToken(Token.TokenType.LITERAL, token.getValue()));
     }
-    return token(Token.TokenType.END, token.getValue());
+    return getToken(Token.TokenType.END, token.getValue());
   }
-
+   
   private String ignoreWhiteSpace() {
     return sourceCode.getInputStream().trim();
   }
 
   private String getInitActiveToken() {
-    if (!isActiveToken()) {
+    if (!isActiveToken())
       setActiveToken(true);
-      char firstLexemeChar = sourceCode.getInputStream().charAt(0);
-      return Character.toString(firstLexemeChar);
-    } else if (sourceCode.getInputStream().isEmpty()) {
-      throw new NullPointerException("Input stream is empty.");
+      return Character.toString(sourceCode.getInputStream().charAt(0));
+  }
+
+  public String moveActiveTokenForward() {
+    for (int i = 0; i < sourceCode.getInputStream().length(); i++) {
+      if (isActiveToken()) {
+        char activeToken = sourceCode.getInputStream().charAt(i);
+        char nextToken = sourceCode.getInputStream().charAt(i+1);
+        activeToken = nextToken;
+        return Character.toString(nextToken);
+      }
+    }   
+  }
+
+  public String moveActiveTokenBackwards() {
+    for (int i = sourceCode.getInputStream().length() -1; i >= 0; i--) {
+      if (isActiveToken()) {
+        char activeToken = sourceCode.getInputStream().charAt(i);
+        char previousToken = sourceCode.getInputStream().charAt(i-1);
+        activeToken = previousToken;
+        return Character.toString(previousToken);
+      }
     }
   }
 
-  public void moveActiveTokenForward() {
-    for (int i = 0; i < tokens.size(); i++)
-      System.out.println(tokens.get(i)); 
+  public String getEndToken() {
+    if (!hasMoreTokens()) 
+      return Character.toString(sourceCode.getInputStream().charAt(sourceCode.getInputStream().length() -1));
   }
-
-  public void moveActiveTokenBackwards() {
-    for (int i = tokens.size() -1; i >= 0; i--)
-      System.out.println(tokens.get(i));
-  }
-
-  public boolean getEndToken() {
-    if (searchPattern.getInputStream().indexOf(0) > searchPattern.getInputStream().length() -1) {
-      return token(tokenType.END, token.getValue());
-    }
-    return false;
-  }
-
-  public boolean hasMoreTokens(ArrayList<String> tokens) {
-    if (tokens.isEmpty()) {
-      return true;
-    } else {
-      return false;
-    }
+  
+  public boolean hasMoreTokens() {
+    return tokens.isEmpty(); 
   }
 }
