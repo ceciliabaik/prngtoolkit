ad2428734a0d1706d2c0902d5e199e7a67645d7f
Cecilia Baik <mb224dt@student.lnu.se>
2022-09-20 21:12:20 +0200

    Update file

--- src/main/java/cryptosystem/model/generator/KeyPairGenerator.java
+++ src/main/java/cryptosystem/model/generator/KeyPairGenerator.java
@@ -1,30 +1,43 @@
-package cryptosystem.generator;
+package cryptosystem.model.generator;
 
 import java.math.BigInteger;
 
 public class KeyPairGenerator {
-    private final PrimalityTest primeOne;
-    private final PrimalityTest primeTwo;
+    private Prime primeOne;
+    private Prime primeTwo;
+    private BigInteger modulus;
+    private BigInteger phi;
 
     public KeyPairGenerator() {
-        primeOne = primeOne.getPrime();
-        primeTwo = primeTwo.getPrime();
+        primeOne = new Prime();
+        primeTwo = new Prime();
     }
 
-    private int generateKeyPair() {
-        while () {
-            this.getModulus();
-            this.getPhi();
-        }
-        return (modulus, publicExponent, privateExponent)
+    public BigInteger getModulus() {
+        return modulus;
+    }
+
+    public BigInteger getPhi() {
+        return phi;
+    }
+
+    private void generateKeyPair() {
+        do {
+            primeOne.getValue();
+            primeTwo.getValue();
+            getKeyLengthForModulus();
+            getCoprimeRangeForPhi();
+        } while (BigInteger.isProbablePrime());
     }
 
     // required bit length of 1024 bits
     private BigInteger getKeyLengthForModulus() {
-        return (this.primeOne).multiply(this.primeTwo);
+        modulus = (primeOne).multiply(primeTwo);
+        return modulus;
     }
 
-    private BigInteger getCoPrimeLengthForPhi() {
-        return (this.primeOne - 1).multiply((this.primeTwo - 1));
+    private BigInteger getCoprimeRangeForPhi() {
+        phi = (primeOne - 1).multiply((primeTwo - 1));
+        return phi;
     }
 }
