a1d752557ac97bf0e411f297372fafff684f9266
Cecilia Baik <mb224dt@student.lnu.se>
2023-09-26 23:15:57 +0200

    Remove rnglibrary folder

--- lib/src/main/java/l1/rnglibrary/generators/RandomNumberGenerator.java
+++
@@ -1,8 +0,0 @@
-package l1.rnglibrary.generators;
-
-public interface RandomNumberGenerator<T> {
-  T generateRandomNumber();
-  T[] generateRandomNumbers(int count);
-  T generateRandomNumberInRange(T min, T max);
-  T[] generateRandomNumbersInRange(T min, T max, int count);
-}
