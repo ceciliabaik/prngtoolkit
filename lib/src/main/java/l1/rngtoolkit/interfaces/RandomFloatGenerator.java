a443b8a7196d8338190f0c61703a1673a09c4e7c
Cecilia Baik <mb224dt@student.lnu.se>
2023-09-27 01:09:02 +0200

    Remove folder interfaces

--- lib/src/main/java/l1/rngtoolkit/interfaces/RandomFloatGenerator.java
+++
@@ -1,8 +0,0 @@
-package l1.rngtoolkit.interfaces;
-
-import l1.rngtoolkit.models.Range;
-
-public interface RandomFloatGenerator {
-  float generateRandomFloatInRange(Range<Float> range);
-  float[] generateRandomFloatsInRangeAndCount(Range<Float> range, int count);
-}
--- lib/src/main/java/l1/rngtoolkit/interfaces/RandomIntegerGenerator.java
+++
@@ -1,8 +0,0 @@
-package l1.rngtoolkit.interfaces;
-
-import l1.rngtoolkit.models.Range;
-
-public interface RandomIntegerGenerator {
-  int generateRandomIntegerInRange(Range<Integer> range);
-  int[] generateRandomIntegersInRangeAndCount(Range<Integer> range, int count);
-}
